4c6bb4d8eeb43a24350f68087e62289bfbe20394
henroj2007 <henroj2007@gmail.com>
2021-04-14 21:52:20 -0500

    inicio Script

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function validardatos(){
+
+    // alert("Hola Mundo");
+     //prompt("Ingrese su nombre");
+     //console.log("Hola Mundo");
+     //document.write("Hola Mundo");
+ 
+     let nombre = document.getElementById(`name`).Value
+ 
+     if(nombre == 'Luis Henry Rojas Munevar'){
+         alert(`Welcome`)
+     }else{
+         alert(`No tienes Acceso`);
+     }
+ 
+ }
